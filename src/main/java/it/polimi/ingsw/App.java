43d0a7df4cc1f3704e103724bb6d9d2e000f5c25
Marco Speziali <marco.speziali@weblink.it>
2018-03-31 17:18:02 +0200

    Committed first version of the UML class diagram (UNFINISCHED)
    Created a basic hierarchy of the it.polimi.ingsw package including:
    it.polimi.ingsw:
    - App
    it.polimi.ingsw.core:
    - Card
    - ToolCard (declaration-only)
    - PublicObjectiveCard
    - PublicObjective
    - PrivateObjectiveCard
    - GlassColor
    - it.polimi.ingsw.core.text
    -- LocalizedString
    -- Cache<K, V>
    it.polimi.ingsw.ui
    - Window

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -4,7 +4,7 @@ package it.polimi.ingsw;
  * Hello world!
  */
 public class App {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
