f1a801539270b33148cb4f46bbadf2a93d07ac9c
Unknown <10492455@polimi.it>
2018-05-08 15:44:34 +0200

    Added some test for window

--- lib/src/test/java/it/polimi/ingsw/models/WindowTest.java
+++ lib/src/test/java/it/polimi/ingsw/models/WindowTest.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.models;
 
 import it.polimi.ingsw.core.GlassColor;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,40 +18,44 @@ class WindowTest {
 
         this.cells = new Cell[3][4];
 
-        for (int i = 0; i < cells.length; i++) {
-            for (int j = 0; j < cells[0].length; j++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
                 cells[i][j] = new Cell(0, null);
             }
 
         }
 
         cells[0][1] = new Cell(5, null);
-        cells[0][4] = new Cell(0, GlassColor.GREEN);
+        cells[0][3] = new Cell(0, GlassColor.GREEN);
         cells[1][2] = new Cell(2, null);
         cells[1][3] = new Cell(3, GlassColor.PURPLE);
         cells[2][0] = new Cell(0, GlassColor.BLUE);
         cells[2][1] = new Cell(2, null);
         cells[2][2] = new Cell(0, GlassColor.RED);
         cells[2][3] = new Cell(0, GlassColor.YELLOW);
-        cells[2][4] = new Cell(3, null);
-        cells[3][2] = new Cell(4, null);
+        cells[2][3] = new Cell(3, null);
+        cells[0][2] = new Cell(4, null);
 
-        this.window = new Window(22, 5, 4, "test", null, cells);
+        this.window = new Window(22, 3, 4, "test", null, cells);
     }
     @Test
-    void getDifficulty() {
-    }
-
-    @Test
-    void getId() {
+    void getDifficultyTest() {
+        Assertions.assertEquals(22, window.getDifficulty());
     }
 
     @Test
-    void getSibling() {
+    void getIdTest() {
+        Assertions.assertEquals("test", window.getId());
     }
 
     @Test
-    void getCells() {
+    void getSiblingTest() {
+        Assertions.assertNull(window.getSibling());
+    }
+
+    @Test
+    void getCellsTest() {
+        Assertions.assertEquals(cells, window.getCells());
     }
 
     @Test
