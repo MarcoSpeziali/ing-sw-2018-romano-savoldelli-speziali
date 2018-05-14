dd249a31381e5c6647050c4c7e3a6c8c4f7eb41c
rluca96 <10492455@polimi.it>
2018-05-15 00:58:08 +0200

    card tests passed

--- lib/src/test/java/it/polimi/ingsw/models/CellTest.java
+++ lib/src/test/java/it/polimi/ingsw/models/CellTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class CellTest {
+class   CellTest {
 
     private Cell coloredCell;
     private Cell shadedCell;
