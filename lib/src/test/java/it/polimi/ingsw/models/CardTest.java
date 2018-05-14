55ed12c8033bb3831b9fd63476e8602e5a722c8f
rluca96 <10492455@polimi.it>
2018-05-15 00:56:56 +0200

    card tests passed

---
+++ lib/src/test/java/it/polimi/ingsw/models/CardTest.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw.models;
+
+import it.polimi.ingsw.core.Effect;
+import it.polimi.ingsw.utils.text.LocalizedString;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class CardTest {
+    private ToolCard toolCard;
+    private Effect effect;
+    private Image image;
+    private LocalizedString title_test;
+    private LocalizedString description_test;
+
+    @BeforeEach
+    void setUp() {
+
+        this.image = mock(Image.class);
+        this.effect = mock(Effect.class);
+        this.description_test = mock(LocalizedString.class);
+        this.title_test = mock(LocalizedString.class);
+
+        this.toolCard = new ToolCard("testId", "test_card", 3, effect , image,
+               image,  title_test, description_test);
+    }
+
+    @Test
+    void getBackImageTest() {
+       Assertions.assertEquals(image, toolCard.getBackImage());
+    }
+
+    @Test
+    void getFrontImageTest() {
+        Assertions.assertEquals(image, toolCard.getFrontImage());
+    }
+
+    @Test
+    void getTitleTest() {
+        Assertions.assertEquals(title_test, toolCard.getTitle());
+    }
+
+    @Test
+    void getDescriptionTest() {
+        Assertions.assertEquals(description_test, toolCard.getDescription());
+    }
+}
