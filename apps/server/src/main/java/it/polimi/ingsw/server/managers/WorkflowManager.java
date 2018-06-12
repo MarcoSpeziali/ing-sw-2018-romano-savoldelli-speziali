bc34326b81803be1c9bb61941d186729c455e843
Davide Savoldelli <davide.savoldelli@mail.polimi.it>
2018-06-12 02:01:52 +0200

    Added WorkflowManager, Turn classes (need review)

--- apps/server/src/main/java/it/polimi/ingsw/server/managers/WorkflowManager.java
+++ apps/server/src/main/java/it/polimi/ingsw/server/managers/WorkflowManager.java
@@ -1,4 +1,12 @@
 package it.polimi.ingsw.server.managers;
 
 public class WorkflowManager {
+    private static WorkflowManager ourInstance = new WorkflowManager();
+
+    public static WorkflowManager getInstance() {
+        return ourInstance;
+    }
+
+    private WorkflowManager() {
+    }
 }
--- lib/src/main/java/it/polimi/ingsw/core/Player.java
+++ lib/src/main/java/it/polimi/ingsw/core/Player.java
@@ -2,8 +2,15 @@ package it.polimi.ingsw.core;
 
 import it.polimi.ingsw.core.locations.RandomPickLocation;
 import it.polimi.ingsw.core.locations.RandomPutLocation;
+import it.polimi.ingsw.listeners.OnDiePickedListener;
+import it.polimi.ingsw.listeners.OnDiePutListener;
+import it.polimi.ingsw.listeners.OnToolCardUsedListener;
 import it.polimi.ingsw.models.Die;
 import it.polimi.ingsw.models.ObjectiveCard;
+import it.polimi.ingsw.models.ToolCard;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Player implements RandomPickLocation, RandomPutLocation {
     private static final long serialVersionUID = -2840357342425816145L;
@@ -14,6 +21,10 @@ public class Player implements RandomPickLocation, RandomPutLocation {
     private Die pickedDie;
 
     private static Player currentPlayer;
+
+    List<OnDiePickedListener> onDiePickedListeners = new LinkedList<>();
+    List<OnDiePutListener> onDiePutListeners = new LinkedList<>();
+    List<OnToolCardUsedListener> onToolCardUsedListeners = new LinkedList<>();
 
     public static Player getCurrentPlayer() {
         return currentPlayer;
@@ -75,10 +86,17 @@ public class Player implements RandomPickLocation, RandomPutLocation {
         this.favourTokenCount = favourTokenCount;
     }
 
+    public void useToolCard(ToolCard toolCard) {
+        toolCard.activate();
+        this.onToolCardUsedListeners.forEach(listener -> listener.onToolCardUsed(toolCard));
+    }
+
+
     @Override
     public Die pickDie() {
         Die die = this.pickedDie;
         this.pickedDie = null;
+        this.onDiePickedListeners.forEach(listener -> listener.onDiePicked(die));
 
         return die;
     }
@@ -91,10 +109,24 @@ public class Player implements RandomPickLocation, RandomPutLocation {
     @Override
     public void putDie(Die die) {
         this.pickedDie = die;
+        this.onDiePutListeners.forEach(listener -> listener.onDiePut(die));
     }
 
     @Override
     public int getFreeSpace() {
         return this.pickedDie == null ? 1 : 0;
+    }
+
+
+    public void addListener(OnDiePickedListener onDiePickedListener) {
+        this.onDiePickedListeners.add(onDiePickedListener);
+    }
+
+    public void addListener(OnDiePutListener onDiePutListener) {
+        this.onDiePutListeners.add(onDiePutListener);
+    }
+
+    public void addListener(OnToolCardUsedListener onToolCardUsedListener) {
+        this.onToolCardUsedListeners.add(onToolCardUsedListener);
     }
 }
